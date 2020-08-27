3d6807f46e3eacbe606a84ab4a57d705bb5d8a4f
Eduardo Oliveira <49571967+eduardoliv@users.noreply.github.com>
2020-08-27 19:57:48 +0100

    Version 0.0.3 | 27 Aug 2020
    Navbar closing as links are clicked.
    Minor improvments.

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,8 +1,13 @@
 function navbar() {
-    if (window.screen.availWidth >= 760) {
+    if (window.screen.availWidth > 768) {
         if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
             document.getElementById("navbar").style.backgroundColor = "#1c073b";
         else
             document.getElementById("navbar").style.backgroundColor = "transparent";
     }
 }
+
+function menuHide(e) {
+    if (e.target.matches('.link'))
+        document.getElementById('menu-btn').checked = false;
+}    
