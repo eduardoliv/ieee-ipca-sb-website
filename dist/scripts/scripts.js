4438b52a560bb9516730bc6e9aefdc16c0bae917
Eduardo Oliveira <49571967+eduardoliv@users.noreply.github.com>
2020-08-27 22:34:07 +0100

    Version 0.0.3 | 27 Aug 2020
    Navbar open/close corrected.
    Minor fixes.

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,13 +1,15 @@
 function navbar() {
-    if (window.screen.availWidth > 768) {
-        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
             document.getElementById("navbar").style.backgroundColor = "#1c073b";
-        else
+    else
             document.getElementById("navbar").style.backgroundColor = "transparent";
-    }
 }
 
 function menuHide(e) {
     if (e.target.matches('.link'))
         document.getElementById('menu-btn').checked = false;
+    else if (e.target.matches('.menu-btn')){
+        document.getElementById("navbar").style.backgroundColor = "#1c073b";
+        document.getElementById("menu").style.backgroundColor = "#1e093d";
+    }
 }    
