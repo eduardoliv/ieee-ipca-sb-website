ce870c2fe3db4d1f65d861f496c58650d2be8fd5
Eduardo Oliveira <49571967+eduardoliv@users.noreply.github.com>
2020-09-02 22:49:28 +0100

    Version 1.0.0
    Menu problems with input state solved.
    To do: Improve the navbar on mobile.

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,15 +1,24 @@
-function rolldown() {
+function rollDown() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
             document.getElementById("navbar").style.backgroundColor = "#110626";
     else
             document.getElementById("navbar").style.backgroundColor = "transparent";
 }
 
-function menuHide(e) {
-    if (e.target.matches('.link'))
-        document.getElementById('menu-btn').checked = false;
-    else if (e.target.matches('.menu-btn')){
+function menuHide() {
+    if(window.innerWidth <= 799){
+        document.getElementById('menu-btn').checked = 0;
+        document.body.classList.toggle('lock-scroll');
+    }
+}
+
+function lockScroll() {
+    if (document.getElementById('menu-btn').checked == 1){
         document.getElementById("navbar").style.backgroundColor = "#110626";
         document.getElementById("menu").style.backgroundColor = "#110626";
+        document.body.classList.toggle('lock-scroll');
     }
-}    
+    else if (document.getElementById('menu-btn').checked == 0){
+        document.body.classList.toggle('lock-scroll');
+    }
+}
