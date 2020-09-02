c6b1bb6beec260e5e7a17c8a8dbd52c73b166b96
Eduardo Oliveira <49571967+eduardoliv@users.noreply.github.com>
2020-09-02 19:10:26 +0100

    Version 0.0.3

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,6 +1,6 @@
-function navbar() {
+function rolldown() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300)
-            document.getElementById("navbar").style.backgroundColor = "#1c073b";
+            document.getElementById("navbar").style.backgroundColor = "#110626";
     else
             document.getElementById("navbar").style.backgroundColor = "transparent";
 }
@@ -9,7 +9,7 @@ function menuHide(e) {
     if (e.target.matches('.link'))
         document.getElementById('menu-btn').checked = false;
     else if (e.target.matches('.menu-btn')){
-        document.getElementById("navbar").style.backgroundColor = "#1c073b";
-        document.getElementById("menu").style.backgroundColor = "#1e093d";
+        document.getElementById("navbar").style.backgroundColor = "#110626";
+        document.getElementById("menu").style.backgroundColor = "#110626";
     }
 }    
